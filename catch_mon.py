a737293e1fe316423053afeece9edd7bd9461732
Rich Reneau <rich.reneau@gmail.com>
2017-11-16 15:59:32 -0500

    set up classes and positioned the fighters

--- catch_mon.py
+++ catch_mon.py
@@ -10,8 +10,23 @@ def main():
     pygame.display.set_caption('My Game')
     clock = pygame.time.Clock()
     
+    class Fighters(object):
+        def __init__(self, name):
+            self.name = name
+
+    class Hero(Fighters):
+        def screen_pos(self):
+            hero_image = pygame.image.load('images/hero.png')        
+            screen.blit(hero_image, (240, 220))
+
+    class Monster(Fighters):
+        def screen_pos(self):
+            hero_image = pygame.image.load('images/monster.png')        
+            screen.blit(hero_image, (125, 350))
 
     # Game initialization
+    hero = Hero('You')
+    monster = Monster('monster')
 
     stop_game = False
     while not stop_game:
@@ -31,9 +46,13 @@ def main():
         screen.fill(blue_color)
         background_image = pygame.image.load('images/background.png')
         screen.blit(background_image, (0, 0))
+        
+        
 
 
         # Game display
+        hero.screen_pos()
+        monster.screen_pos()
 
         pygame.display.update()
         clock.tick(60)
